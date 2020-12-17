3b893ea7c0d98e202162d365549292bd3aa081e5
EricShi1255 <eshi20@stuy.edu>
2020-12-16 23:41:52 -0500

    2 test cases

--- SortTester.java
+++ SortTester.java
@@ -2,8 +2,10 @@ import java.util.Arrays;
 public class SortTester {
     public static void main(String args[]) {
         int[] A = {3, 7, 5, 9};
-        System.out.println(Arrays.toString(A));
         Sorts.bubbleSort(A);
-        System.out.println(Arrays.toString(A));
+        System.out.println("{3, 7, 5, 9} --> " + Arrays.toString(A));
+        int[] B = {-3, 0, 2, 5, 7, 9};
+        Sorts.bubbleSort(B);
+        System.out.println("{-3, 0, 2, 5, 7, 9} --> " + Arrays.toString((B)));
     }
 }
