4e5908541b54f3e4ddd0a6adc6737ea808e68c46
EricShi1255 <eshi20@stuy.edu>
2020-12-20 01:00:03 -0500

    well it compiles... .-.

--- Sorts.java
+++ Sorts.java
@@ -20,8 +20,24 @@ public class Sorts{
           prevListSwap = false;
         }
       }
-
     }
+    public static void selectionSort(int[] data){
+      for (int i = 1; i < data.length-1; i++) {
+        int min = data[i];
+        int minpos = i;
+        int j;
+        //find the min
+        for (j = 0; j < data.length-1; j++) {
+          if (data[j] < min) {
+            min = data[j];
+            minpos = j;
 
-    
-  }
+          }
+        }
+      //send min to the front
+      int temp = data[i];
+      data[i] = j;
+      data[minpos] = temp;
+      }
+    }
+}
