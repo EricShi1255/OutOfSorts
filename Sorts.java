10cb08c80a8a819aef695a4d06baba3291db4929
EricShi1255 <eshi20@stuy.edu>
2020-12-20 16:37:58 -0500

    redo

--- Sorts.java
+++ Sorts.java
@@ -4,62 +4,48 @@ public class Sorts{
     *Upon completion, the elements of the array will be in increasing order.
     *@param data  the elements to be sorted.
     */
-    public static void bubbleSort(int[] data) {
-      boolean prevListSwap = true;
-      while (prevListSwap && (data.length != 0)) {
-        boolean swapped = false; 
-        for (int i = 0; i < data.length-1; i++) {   
-          if (data[i] > data[i+1]) {
-            int curr = data[i];
-            int next = data[i+1];
-            data[i] = next;
-            data[i+1] = curr;
-            swapped = true;
-          }
-        }
-        if (!swapped) {
-          prevListSwap = false;
+  public static void bubbleSort(int[] data) {
+    boolean prevListSwap = true;
+    while (prevListSwap && (data.length != 0)) {
+      boolean swapped = false; 
+      for (int i = 0; i < data.length-1; i++) {   
+        if (data[i] > data[i+1]) {
+          int curr = data[i];
+          int next = data[i+1];
+          data[i] = next;
+          data[i+1] = curr;
+          swapped = true;
         }
       }
+      if (!swapped) {
+        prevListSwap = false;
+      }
     }
-    public static void selectionSort(int[] data){
-      int i;
-      int j;
-      for (i = 0; i < data.length-1; i++) {
-        int min = data[i];
-        int minpos = i;
-        //find the min
-        for (j = i; j < data.length; j++) {
-          if (data[j] < min) {
-            min = data[j];
-            minpos = j;
-          }
+  }
+  public static void selectionSort(int[] data){
+    int i;
+    int j;
+    for (i = 0; i < data.length-1; i++) {
+      int min = data[i];
+      int minpos = i;
+      //find the min
+      for (j = i; j < data.length; j++) {
+        if (data[j] < min) {
+          min = data[j];
+          minpos = j;
         }
+      }
       //send min to the front
-      int temp = data[i];
-      data[i] = min;
-      data[minpos] = temp;
-      }
+    int temp = data[i];
+    data[i] = min;
+    data[minpos] = temp;
     }
-    public static void arrayInsert(int[] array, int pos, int num ) {
-      int curr;
-      int prev= array[pos];
-      array[pos] = num;
-      for (int i = pos+1; i < array.length; i++) {
-        curr = array[i];
-        array[i] = prev;
-        prev = curr;
-      }
-    }
+  }
+   
 
-    public static void insertionSort(int[] data){
-      for (int i = 1; i < data.length; i++) {
-        for (int j = i-1; j > 0; j--) {
-          if (data[i] > data[j]) {
-            arrayInsert(data, j, data[i]);
-            
-          }
-        }
-      }
+  public static void insertionSort(int[] data){
+    for (int i = 1; i < data.length; i++) {
+        
     }
+  }
 }
